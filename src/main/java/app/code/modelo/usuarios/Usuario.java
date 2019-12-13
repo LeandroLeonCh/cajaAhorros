d9789a7b174e4a81ae227066f0754cc7027afc75
Angel Mesias Jadan Corte <ajadanc@est.ups.edu.ec>
2019-12-13 12:25:53 -0500

    -CRUD Administracion de usuarios(Listar)

--- src/main/java/app/code/modelo/usuarios/Usuario.java
+++ src/main/java/app/code/modelo/usuarios/Usuario.java
@@ -16,27 +16,75 @@ import javax.persistence.Column;
  * @author EstAngelMesiasJadanC
  */
 @Entity
-@Table(name="BG_USUARIOS")
-public class Usuario extends Auditoria implements Serializable{
-    
-    @Column(name="username")
+@Table(name = "BG_USUARIOS")
+public class Usuario extends Auditoria implements Serializable {
+
+    @Column(name = "username")
     private String usuario;
-    
-    @Column(name="password")
+
+    @Column(name = "password")
     private String contrasenia;
-    
-    @Column(name="nombre")
+
+    @Column(name = "nombre")
     private String nombre;
-    
-    @Column(name="apellido")
+
+    @Column(name = "apellido")
     private String apellido;
-    
-    @Column(name="es_admin")
+
+    @Column(name = "es_admin")
     private boolean esAdmin;
-        
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isEsAdmin() {
+        return esAdmin;
+    }
+
+    public void setEsAdmin(boolean esAdmin) {
+        this.esAdmin = esAdmin;
+    }
+
     public Usuario(Long id, boolean activo) {
         super(id, activo);
     }
-    
-    
+
+    public Usuario(String usuario, String contrasenia, String nombre, String apellido, boolean esAdmin, Long id, boolean activo) {
+        super(id, activo);
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.esAdmin = esAdmin;
+    }
+
 }
