f32fac2294adc0093857fda68309ca495fd5d847
crlos <cmorocho_new@outlook.com>
2019-12-20 12:44:00 -0500

    Agregacion modulo de General

--- src/main/java/app/code/modelo/administracion/Perfil.java
+++ src/main/java/app/code/modelo/administracion/Perfil.java
@@ -6,25 +6,69 @@
 package app.code.modelo.administracion;
 
 import app.code.modelo.Auditoria;
+import java.io.Serializable;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
- *
+ * Clase para mappear la tabla perfiles
  * @author Carlos
  */
-public class Perfil extends Auditoria {
+@Entity
+@Table(name="bg_perfiles")
+public class Perfil extends Auditoria implements Serializable{
     
-    @Column(name="codigo", unique = true)
+    @Column(name="codigo", unique=true)
     private String codigo;
     
-    @Column(name="codigo", unique = true)
+    @Column(name="nombre")
     private String nombre;
     
-    @Column(name="codigo", unique = true)
+    @Column(name="descripcion")
     private String descripcion;
-
-    public Perfil(Long id, boolean activo) {
+    
+    public Perfil() {
       
     }
+     
+    public Perfil(Long id, boolean activo, String codigo, String nombre, String descripcion) {
+        super.setId(id);
+        super.setActivo(activo);
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + (codigo != null ? codigo : "") + " ] - " + (nombre !=null ? nombre : "");
+    }
+    
+    
     
 }
